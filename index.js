145ca54fae0395476f1ae986c0e4e0c4dfc6fd5e
Graham Mendick <graham.mendick@gmail.com>
2021-07-05 14:26:18 +0100

    Moved outside of timeout

--- index.js
+++ index.js
@@ -6,28 +6,26 @@ docViewer.setScrollViewElement(document.getElementById('scrollView'))
 docViewer.setViewerElement(document.getElementById('docViewer'))
 docViewer.setOptions({ enableAnnotations: true })
 docViewer.on('documentLoaded', () => {
-    setTimeout(() => {
-        docViewer.setFitMode(docViewer.FitMode.FitWidth)
-        const annotManager = docViewer.getAnnotationManager()
-        const annotation = new Annotations.TextHighlightAnnotation()
-        annotation.PageNumber = 7
-        annotation.FillColor = 'rgba(12, 105, 255, 0.3)'
-        annotation.Quads = [{
-            x1: 108,
-            x2: 134.2204473888,
-            x3: 134.2204473888,
-            x4: 108,
-            y1: 243.3255354687501,
-            y2: 243.3255354687501,
-            y3: 231.94855760000007,
-            y4: 231.94855760000007
-        }]
-        annotManager.addAnnotation(annotation)
-        annotManager.drawAnnotations(7)
-        annotManager.selectAnnotation(annotation)
-        annotManager.bringToBack(annotation)
-        annotManager.jumpToAnnotation(annotation)
-    }, 2000)
+    docViewer.setFitMode(docViewer.FitMode.FitWidth)
+    const annotManager = docViewer.getAnnotationManager()
+    const annotation = new Annotations.TextHighlightAnnotation()
+    annotation.PageNumber = 7
+    annotation.FillColor = 'rgba(12, 105, 255, 0.3)'
+    annotation.Quads = [{
+        x1: 108,
+        x2: 134.2204473888,
+        x3: 134.2204473888,
+        x4: 108,
+        y1: 243.3255354687501,
+        y2: 243.3255354687501,
+        y3: 231.94855760000007,
+        y4: 231.94855760000007
+    }]
+    annotManager.addAnnotation(annotation)
+    annotManager.drawAnnotations(7)
+    annotManager.selectAnnotation(annotation)
+    annotManager.bringToBack(annotation)
+    annotManager.jumpToAnnotation(annotation)
 })
 docViewer.loadDocument('learn-to-summarize.pdf', {
     filename: 'learn to summarize',
